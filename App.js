5339def1711117aa414b27d96372921d2a63391b
Aman Deep <mndeep204@gmail.com>
2021-07-03 19:40:23 +0530

    Tab navigation is configured

--- App.js
+++ App.js
@@ -8,30 +8,33 @@
 
 import React from 'react';
 import MainStack from './app/routing/MainStack';
-import { Provider } from "react-redux"
-import { StatusBar } from 'react-native'
+import {Provider} from 'react-redux';
+import {StatusBar} from 'react-native';
 import storePre from './app/redux/store';
 import DropdownAlert from 'react-native-dropdownalert';
-import { AlertHelper } from './app/utils/AlertHelper';
-import { PersistGate } from 'redux-persist/integration/react'
-
+import {AlertHelper} from './app/utils/AlertHelper';
+import {PersistGate} from 'redux-persist/integration/react';
+import TabNavigationStack from './app/routing/TabNavigationStack';
+import {navigationTypeTabs} from './app.json';
 const App: () => React$Node = () => {
-  const { persistor, store } = storePre;
+  const {persistor, store} = storePre;
 
   return (
     <Provider store={store}>
-       <PersistGate loading={null} persistor={persistor}>
-      <MainStack/>
-      <DropdownAlert
-          defaultContainer={{ padding: 8, paddingTop: StatusBar.currentHeight, flexDirection: 'row' }}
-          ref={ref => AlertHelper.setDropDown(ref)}
+      <PersistGate loading={null} persistor={persistor}>
+        {navigationTypeTabs ? <TabNavigationStack /> : <MainStack />} 
+        <DropdownAlert
+          defaultContainer={{
+            padding: 8,
+            paddingTop: StatusBar.currentHeight,
+            flexDirection: 'row',
+          }}
+          ref={(ref) => AlertHelper.setDropDown(ref)}
           onClose={() => AlertHelper.invokeOnClose()}
         />
-        </PersistGate>
+      </PersistGate>
     </Provider>
-  )
-
+  );
 };
-
 
 export default App;
